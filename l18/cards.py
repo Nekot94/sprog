66493ebe628a02ab8631197e923609456d2133c2
Nekot94 <nekomato9@gmail.com>
2017-04-21 15:06:16 +0300

    Add files via upload

--- l18/cards.py
+++ l18/cards.py
@@ -4,10 +4,26 @@ def get_card(player):
     while True:
         yield player["Колода"].pop()
 
+def show_card(card):
+    c = card[1]
+    if c == 11:
+        c = "Валет"
+    elif c == 12:
+        c = "Дама"
+    elif c == 13:
+        c = "Король"
+    elif c == 14:
+        c = "Туз"
+    print("{0} - {1}".format(c,card[0]))
+
 def step(player1, player2):
     k = 0
     for card1, card2 in zip(get_card(player1),get_card(player2)):
-        print(card1, card2)
+        #print(card1, card2)
+        print("У первого игрока вышло: ")
+        show_card(card1);
+        print("У второго игрока вышло: ")
+        show_card(card2);
         k += 2
         if card1[1] > card2[1]:
             player1["Счет"] += k
@@ -30,9 +46,15 @@ player2["Колода"] = deck[len(deck) // 2:]
 player1["Счет"] = 0
 player2["Счет"] = 0
 
-print(player1["Колода"], player2["Колода"])
+# print(player1["Колода"], player2["Колода"])
 
-while player1["Колода"] and player2["Колода"]:
+while True:
     step(player1,player2)
     print(player1["Счет"],player2["Счет"])
     input()
+    if not player1["Колода"]:
+        print("Победил второй игрок")
+        break
+    if not player2["Колода"]:
+        print("Победил первый игрок")
+        break
